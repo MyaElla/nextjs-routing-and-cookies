674a2a250e5ace879a4c348b10eecb38d87d0d1f
MyaElla <mihaela.ciobanu03@gmail.com>
2022-05-16 10:27:37 +0100

    Add Routing link back home

--- pages/member/[id]/index.js
+++ pages/member/[id]/index.js
@@ -1,11 +1,23 @@
 import Layout from "/components/layout";
 import Link from "next/link";
+// import Image from "next/image";
 
-export const castMember = ({ castMember }) => {
+const castMember = ({ castMember }) => {
+  console.log(castMember);
   return (
     <Layout>
       <main className="container">
         <h1>{castMember.name}</h1>
+        {/* <Image
+          src={`/images/${castMember.slug}.svg`}
+          alt={castMember.name}
+          width={600}
+          height={800}
+        /> */}
+        <p>{castMember.bio}</p>
+        <Link href="/">
+          <button>Back to HOME</button>
+        </Link>
       </main>
     </Layout>
   );
@@ -14,10 +26,13 @@ export const castMember = ({ castMember }) => {
 export const getServerSideProps = async (context) => {
   const res = await fetch("https://pixelprowess.com/i/stargazers/data.json");
   const data = await res.json();
-
   const castMember = data.filter((item) => item.id == context.params.id);
 
-  return { props: { castMember: castMember[0] } };
+  return {
+    props: {
+      castMember: castMember[0]
+    }
+  };
 };
 
 export default castMember;
